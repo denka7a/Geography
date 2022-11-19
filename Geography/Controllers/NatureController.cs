709ef684e5ded67e503eae42c6a7694cc714b58e
Denislav <dmgdenka7a@abv.bg>
2022-11-19 14:31:56 +0200

    Adding nature controller, model and view

---
+++ Geography/Controllers/NatureController.cs
@@ -0,0 +1,52 @@
+﻿using Geography.Data.Data;
+using Geography.Data.Data.Models;
+using Geography.Models.Nature;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Geography.Controllers
+{
+    public class NatureController : Controller
+    {
+        private readonly GeographyDbContext context;
+
+        public NatureController(GeographyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(NatureViewModel natureModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(natureModel); 
+            }
+
+            var natureType = this.context.NatureTypes.FirstOrDefault(x => x.Type.ToLower() == natureModel.NatureType.ToLower());
+
+            if (natureType == null)
+            {
+                return View(natureModel);
+            }
+
+            var natureObject = new NatureObject()
+            {
+                Name = natureModel.Name,
+                Information = natureModel.Information,
+                URL = natureModel.URL,
+                NatureType = natureType
+            };
+
+            this.context.Add(natureObject);
+            this.context.SaveChanges();
+
+            return RedirectToAction(nameof(Add));
+        }
+    }
+}
---
+++ Geography/Models/Nature/NatureViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Geography.Models.Nature
+{
+    public class NatureViewModel
+    {
+        public string Name { get; set; }
+        public string Information { get; set; }
+        public string URL { get; set; }
+        public string NatureType { get; set; }
+    }
+}

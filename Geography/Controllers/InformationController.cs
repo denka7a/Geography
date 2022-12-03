ecb8ed3499d2566d3b2ef0a80f9aee2a7a2ff313
Denislav <dmgdenka7a@abv.bg>
2022-12-03 02:55:03 +0200

    Adding InformationController

---
+++ Geography/Controllers/InformationController.cs
@@ -0,0 +1,49 @@
+﻿using Geography.Data.Data;
+using Geography.Data.Data.Models;
+using Geography.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geography.Controllers
+{
+    public class InformationController : Controller
+    {
+        private readonly GeographyDbContext context;
+
+        public InformationController(GeographyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Info()
+        {
+            var messages = context.Message.ToList();
+            ViewData["messages"] = messages;
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Info(MessageViewModel messageModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(messageModel);
+            }
+
+            var name = this.User.Identity.Name;
+            var user = this.context.Users.First(x => x.UserName == name);
+
+            var message = new Message()
+            {
+                Id = messageModel.Id,
+                Writer = messageModel.Writer,
+                Text = messageModel.Text,
+                geographyUserId = user.Id
+            };
+
+            context.Message.Add(message);
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Info));
+        }
+    }
+}
---
+++ Geography/Models/Message/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Geography.Models.Message
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        [MinLength(3)]
+        public string Writer { get; set; }
+        [Required]
+        [MaxLength(300)]
+        [MinLength(3)]
+        public string Text { get; set; }
+    }
+}
